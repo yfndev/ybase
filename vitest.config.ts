73eecd69267a171cf09204842d291d8a2faa2260
Luca Kammerer <lucakammerer2@gmail.com>
2026-07-11 09:57:46 +0200

    fix pull request test setup

--- vitest.config.ts
+++ vitest.config.ts
@@ -3,7 +3,6 @@ import { defineConfig } from "vitest/config";
 export default defineConfig({
   test: {
     environment: "node",
-    globalSetup: "./vitest.globalSetup.ts",
     include: ["app/**/*.test.ts"],
     fileParallelism: !process.env.CI,
     env: {
--- vitest.globalSetup.ts
+++
@@ -1,6 +0,0 @@
-import { MongoMemoryServer } from "mongodb-memory-server";
-
-export default async function prepareMongoBinary() {
-  const mongo = await MongoMemoryServer.create();
-  await mongo.stop();
-}
